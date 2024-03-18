f34a32f99b95c91119404d8e9f2376a3ff53a5fa
Raphael Marot <rmarot@jm-a.be>
2024-03-18 15:53:47 +0100

    pkg name

--- dist/module.mjs
+++ dist/module.mjs
@@ -1,12 +1,16 @@
-import jiti from "file:///C:/dev/nuxt3-aos/node_modules/.pnpm/jiti@1.21.0/node_modules/jiti/lib/index.js";
+import { defineNuxtModule, createResolver, addPlugin } from '@nuxt/kit';
 
-/** @type {import("C:/dev/nuxt3-aos/src/module")} */
-const _module = jiti(null, {
-  "esmResolve": true,
-  "interopDefault": true,
-  "alias": {
-    "nuxt3-aos": "C:/dev/nuxt3-aos"
+const module = defineNuxtModule({
+  meta: {
+    name: "nuxt3-aos",
+    configKey: "nuxt3Aos"
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+    addPlugin(resolver.resolve("./runtime/plugin"));
   }
-})("C:/dev/nuxt3-aos/src/module.ts");
+});
 
-export default _module;
+export { module as default };
